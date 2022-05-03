751af23bd883a0ba9c7b0cda3bacfebb1b5bba68
hkapgar <104778569+hkapgar@users.noreply.github.com>
2022-05-03 10:51:02 -0400

    Created scenes and started scene change arrows

---
+++ sketch.js
@@ -0,0 +1,33 @@
+kaboom({
+  background: [225,225,225]
+})
+
+
+
+scene("window", () => {
+  add([
+    rect(100,50),
+    area(),
+    pos(50,550),
+    color(255, 245, 186),
+    "left"
+  ])
+})
+
+scene("bed", () => {
+  
+})
+
+scene("closet", () => {
+  
+})
+
+scene("menu", () => {
+  
+})
+
+scene("end", () => {
+  
+})
+
+go("window")
